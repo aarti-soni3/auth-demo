f11fa040ec4bb3a08fadd5c5e7e5f1e6fa34d167
aarti soni <aartisoni32748@gmail.com>
2026-03-19 15:27:23 +0530

    env changes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     secure: false,
 }
